06a5a19637c7e4b56fddc25181e30364f84e1fb7
iamonuwa <matrix4u2002@gmail.com>
2024-01-05 13:17:21 +0000

    chore(routes): add support for entrypoint addresses

--- src/routes/request/index.ts
+++ src/routes/request/index.ts
@@ -1,6 +1,6 @@
 import debugUserOp from '../../controller/DebugUserOpController';
 import { jsonRPCRouter } from '../../config/router';
-import { supportedNetworks } from '../../config';
+import { networkConfig } from '../../config/network';
 
 jsonRPCRouter.method("eth_debugUserOperation", async (ctx: any) => {
     const params = ctx.request.body.params;
@@ -14,9 +14,20 @@ jsonRPCRouter.method("eth_debugUserOperation", async (ctx: any) => {
 // Add other rpc methods here as mentioned above
 jsonRPCRouter.method("eth_config", async (ctx: any) => {
     ctx.body = {
-        supportedNetworks,
     };
     ctx.status = 200;
 });
+
+
+jsonRPCRouter.method("eth_supportedEntryPoints", async (ctx: any) => {
+    const networkId = ctx.params.networkId;
+    const data = [{
+        label: "EntrypointV6",
+        value: networkConfig[networkId].entryPointV6
+    }]
+    ctx.body = data;
+    ctx.status = 200;
+});
+
 
 export default jsonRPCRouter;
